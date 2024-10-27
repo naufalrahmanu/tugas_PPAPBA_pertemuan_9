b7727267ecad69b0270d99c7a92fa4dfe03a6533
naufalrahmanu <muhammadnaufalrahmanuanindito@mail.ugm.ac.id>
2024-10-27 20:07:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas_p9/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tugas_p9
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tugas_p9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+        }
+    }
+}
